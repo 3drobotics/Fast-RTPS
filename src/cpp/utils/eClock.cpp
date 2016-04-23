1cf4a498b71180526b397ee6efc5208c7b070299
John Finley <jfinley@3drobotics.com>
2016-04-23 09:39:07 -0700

    eClock: add MONOTONIC option
    This adds a second optional argument to setTimeNow that lets one
    use timestamps from the system's monotonic clock instead of the
    wall-clock time.
    
    Monotonic implemented for OS-X and Linux, but not Windows.

--- src/cpp/utils/eClock.cpp
+++ src/cpp/utils/eClock.cpp
@@ -12,6 +12,14 @@
  */
 #include <cmath>
 #include <iostream>
+
+#if (defined __MACH__ && defined __APPLE__)
+#include <mach/mach_time.h>
+#include <sys/time.h>
+#else
+#include <time.h>
+#endif
+
 #include <fastrtps/utils/eClock.h>
 using namespace eprosima::fastrtps::rtps;
 
@@ -35,8 +43,11 @@ eClock::~eClock() {
 #if defined(_WIN32)
 #include <cstdint>
 
-bool eClock::setTimeNow(Time_t* tnow)
+bool eClock::setTimeNow(Time_t* tnow, ClockId_t clockId)
 {
+
+    if (clockId != REALTIME)
+        return false;
 
     GetSystemTimeAsFileTime(&ft);
     ftlong = ft.dwHighDateTime;
@@ -78,12 +89,43 @@ uint64_t eClock::intervalEnd()
 #else //UNIX VERSION
 #include <unistd.h>
 
-bool eClock::setTimeNow(Time_t* tnow)
+bool eClock::setTimeNow(Time_t* tnow, ClockId_t clockId)
 {
-	gettimeofday(&m_now,NULL);
-	tnow->seconds = m_now.tv_sec+m_seconds_from_1900_to_1970+m_utc_seconds_diff;
-	tnow->fraction = (uint32_t)(m_now.tv_usec*pow(2.0,32)*pow(10.0,-6));
-	return true;
+	if (clockId == REALTIME)
+	{
+		gettimeofday(&m_now,NULL);
+		tnow->seconds = m_now.tv_sec+m_seconds_from_1900_to_1970+m_utc_seconds_diff;
+		tnow->fraction = (uint32_t)(m_now.tv_usec*pow(2.0,32)*pow(10.0,-6));
+		return true;
+	}
+	else if (clockId == MONOTONIC)
+	{
+		constexpr int64_t i10p9 = 1000000000ll;
+		constexpr uint64_t u2p32 = 1ull << 32;
+#if (!defined __MACH__ || !defined __APPLE__)
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		int64_t now_ns = (int64_t)ts.tv_sec * i10p9 + ts.tv_nsec;
+#else
+		// read timebase info from os just once
+		static struct mach_timebase_info tb_info = {0, 0};
+		if (tb_info.denom == 0)
+			mach_timebase_info(&tb_info);
+		int64_t now_ns = (mach_absolute_time() * tb_info.numer) / tb_info.denom;
+#endif
+		tnow->seconds = now_ns / i10p9;
+		// nsec % i10p9 is < i10p9 < 2^31, so the i64 multiply can't overflow
+		uint64_t fraction_ns = now_ns % i10p9;
+		tnow->fraction = (fraction_ns * u2p32 + i10p9 / 2) / i10p9;
+		// XXX does m_now need to be set?
+		m_now.tv_sec = tnow->seconds;
+		m_now.tv_usec = uint32_t(fraction_ns) + 500 / 1000;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 void eClock::my_sleep(uint32_t milliseconds)
